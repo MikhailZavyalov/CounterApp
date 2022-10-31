2ccb60064ffaeb68b76da551366abe7cb950fcbf
Mikhail Zavyalov <mikhailzavyalov0807@gmail.com>
2022-10-31 16:47:31 +0400

    Проект "Счётчик" закончен

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -2,18 +2,27 @@
 //  ViewController.swift
 //  CounterApp
 //
-//  Created by Мария Авдеева on 30.10.2022.
+//  Created by Михаил Завьялов on 30.10.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var plusOneButton: UIButton!
+    var value: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        plusOneButton.tintColor = .red
+        counter.text = "Значение счётчика: \(value)"
     }
-
-
+   
+    @IBAction func plusOneActiveButton(_ sender: Any) {
+        value += 1
+        counter.text = "Значение счётчика: \(value)"
+    }
+    
 }
 
